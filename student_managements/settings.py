9b4e43dc40828fda5e43a466227383710d1c6750
suryaexouzia-beep <suryaexouzia@email.example.com>
2026-08-27 20:57:05 +0400

    Fix PythonAnywhere SQLite database settings

--- student_managements/settings.py
+++ student_managements/settings.py
@@ -19,12 +19,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-!@a-sc_n2q-w(!9gj0q5q3r8#eb^)8i87g@dvvo*vj1c7!za^!'
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 CORS_ALLOW_ALL_ORIGINS = True
+
 ALLOWED_HOSTS = []
 
 
@@ -54,7 +58,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
 ROOT_URLCONF = 'student_managements.urls'
+
 
 TEMPLATES = [
     {
@@ -70,6 +77,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'student_managements.wsgi.application'
 
@@ -122,11 +130,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-# Email
-# https://docs.djangoproject.com/en/6.1/topics/email/#topic-email-configuration
 
-MAILERS = {
-    'default': {
-        'BACKEND': 'django.core.mail.backends.console.EmailBackend',
-    },
-}
+# Email
+# https://docs.djangoproject.com/en/6.1/topics/email/
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
